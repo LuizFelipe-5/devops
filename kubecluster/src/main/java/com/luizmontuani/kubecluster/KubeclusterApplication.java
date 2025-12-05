c49f75df053c23245dde8342298c938fb63d772a
Luiz Montuani <luizmontuani@ioasys.com.br>
2025-12-04 23:28:12 -0300

    feat: Initialize kubecluster project with Gradle wrapper and Spring Boot setup
    - Added Gradle wrapper configuration files (gradle-wrapper.properties, gradlew, gradlew.bat).
    - Created basic Spring Boot application structure with main application class (KubeclusterApplication) and a simple REST controller (HelloController).
    - Defined application properties and settings for the Spring Boot application.
    - Added Kubernetes resource definitions including a Pod and a ReplicaSet for the hello-spring application.
    - Included test class for application context loading verification.

---
+++ kubecluster/src/main/java/com/luizmontuani/kubecluster/KubeclusterApplication.java
@@ -0,0 +1,13 @@
+package com.luizmontuani.kubecluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeclusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeclusterApplication.class, args);
+    }
+
+}
---
+++ kubecluster/src/test/java/com/luizmontuani/kubecluster/KubeclusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmontuani.kubecluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeclusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
